f3a88b8c574ac689f4692610104978a4708e2d30
korarit <44546773+wkorarit@users.noreply.github.com>
2021-05-22 23:09:26 +0700

    Resgistration update

--- registration.py
+++ registration.py
@@ -1,10 +1,18 @@
-temp_var = {
-    "already_get_credit": True
+respond = {
+    "already_get_credit": False
 }
+while 1 :
+    student_id = input("Enter your student ID:")
+    if(student_id.isnumeric() and len(student_id) == 11):
+        break
 
-student_id = input("Enter your student ID:")
 pub = input("Enter your public key:")
 
 request = {
-    "student_id": "xxxxxxx"
+    "student_id": student_id
 }
+
+if(respond["already_get_credit"]==True):
+    print("Success Registration, your account will be ready to vote soon!")
+else:
+    print("You're already complete with this process before")
