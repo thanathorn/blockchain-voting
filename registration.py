3465e0a9db0becc12e37721e69e8b9695259bba1
korarit <44546773+wkorarit@users.noreply.github.com>
2021-05-23 23:37:30 +0700

    update regis post command

--- registration.py
+++ registration.py
@@ -1,18 +1,17 @@
-respond = {
-    "already_get_credit": False
-}
+import requests
+
 while 1 :
     student_id = input("Enter your student ID:")
     if(student_id.isnumeric() and len(student_id) == 11):
         break
-
+url = "http://127.0.0.1:8000/master/registration/student"
+payload={'student_id': '61070501023'}
+files=[]
+headers = {}
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
 pub = input("Enter your public key:")
 
-request = {
-    "student_id": student_id
-}
-
-if(respond["already_get_credit"]==True):
+if response.json()['already_get_credit']==False:
     print("Success Registration, your account will be ready to vote soon!")
 else:
     print("You're already complete with this process before")
