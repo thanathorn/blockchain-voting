dfe5a54899c5dd75230f05f7db219639f6b03532
spicysmall <44726962+spicysmall@users.noreply.github.com>
2021-05-26 16:12:29 +0700

    Update registration.py

--- registration.py
+++ registration.py
@@ -3,7 +3,7 @@ import json
 import pickle
 import socket
 import time
-
+import requests
 import zmq
 
 from helper import printLog
@@ -69,6 +69,8 @@ def broadcastTx(tx):
 printLog("Registration", "Starting...")
 masterAddressListener()
 
+
+
 while 1 :
     registration_id = input("Enter your registration ID:")
     if(registration_id.isnumeric() and 1<=int(registration_id)<=3):
@@ -77,6 +79,18 @@ while 1 :
     student_id = input("Enter your student ID:")
     if(student_id.isnumeric() and len(student_id) == 11):
         break
+
+url = "http://127.0.0.1:8000/master/registration/student"
+payload={'student_id': '{}'.format(student_id)}
+files=[]
+headers = {}
+response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+
+if response.json()['already_get_credit']==False:
+    print("Success Registration, your account will be ready to vote soon!")
+else:
+    print("You're already complete with this process before")
 
 
 public_S = open("./keys/voter/{}/public.pem".format(student_id,student_id),"r")
